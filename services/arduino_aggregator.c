1834d025e41d7d8785fc02a0937d4d51989774e4
Ian Llewellyn <ian_llewellyn@hotmail.com>
2015-06-14 08:35:10 +0100

    Added connector services files.

---
+++ services/arduino_aggregator.c
@@ -0,0 +1,4 @@
+/*
+ * Multiple sensors in
+ * Single JSON transmitted out
+ */
---
+++ services/pi_connector.py
@@ -0,0 +1,2 @@
+# Multiple TCP connections in with JSON data
+# Database INSERT for each sensor (may be multiple sensors per connection)
