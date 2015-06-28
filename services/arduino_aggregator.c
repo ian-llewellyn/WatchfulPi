6746d2925fc87d45693abdcecea1eb8867cbfa26
Ian Llewellyn <ian_llewellyn@hotmail.com>
2015-06-28 12:36:36 +0100

    README: Added JSON Message Format section arduino_aggregator.c: Added the beginings of code (mostly pseudo) nothing tested system_layout: Added file to track sensors, their connections and reading units

--- services/arduino_aggregator.c
+++ services/arduino_aggregator.c
@@ -2,3 +2,49 @@
  * Multiple sensors in
  * Single JSON transmitted out
  */
+
+setup() {
+
+}
+
+loop() {
+    // Measure Oil Level
+    oil_level = hcsr04.get_distance()
+
+    // Measure Temperature
+    temp = bmp180.get_temperature()
+
+    // Measure Pressure
+    pressure = bmp180.get_presure(temp)
+
+    // Battery Voltage
+    voltage = arduino.get_voltage()
+
+
+
+    // Create JSON Message
+    json = """
+{"measurements": [{
+    "id": oil_level.id,
+    "reading": oil_level.value
+},{
+    "id": temp.id,
+    "reading": temp.value
+},{
+    "id": pressure.id,
+    "reading": pressure.value
+},{
+    "id": voltage.id,
+    "reading": voltage.value
+}]}
+    """
+
+    // Transmit JSON Message
+    for (i=0; i<6; i++) {
+        txrx433.transmit(json);
+        sleep(1);
+    }
+
+    // Sleep
+    sleep(53);
+}
