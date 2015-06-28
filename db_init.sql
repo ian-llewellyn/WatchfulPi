2b1b456cc8aaa7e8cc15f14e2266498d60f35683
Ian Llewellyn <ian_llewellyn@hotmail.com>
2015-06-28 11:42:20 +0100

    README: Updated example setup README: Updated 'Database' section db_init.sql: Added initialising SQL

---
+++ db_init.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS 'sensors' (
+    'id' SMALLINT UNSIGNED NOT NULL PRIMARY KEY,
+    'type' CHAR(16) NOT NULL,
+    'location' CHAR(32) NOT NULL,
+    'zone' CHAR(32) NOT NULL,
+    'system' CHAR(32) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS 'measurements' (
+    'id' SMALLINT UNSIGNED NOT NULL PRIMARY KEY,
+    'datetime' TIMESTAMP NOT NULL,
+    'sensor' SMALLINT UNSIGNED NOT NULL,
+    'reading' DECIMAL(7,3) NOT NULL
+);
