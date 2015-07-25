831134c88e634436d3b9320094891f1444677aa4
Ian Llewellyn <ian_llewellyn@hotmail.com>
2015-07-25 11:26:16 +0100

    Added units and title to JSON examples in README file. Added units to DB table description in README file. Split out Web Service Builds in TODO file. Minor change to System Setup page and marked the first batch of work as done on the System Setup page - indicated bu a dot '.' next to it in TODO. Added first bit of boilerplate for WatchfulPi.js file.

---
+++ user_if/js/WatchFulPi.js
@@ -0,0 +1,31 @@
+function Reading() {
+
+}
+Reading.protype.getReading = function() {
+
+}
+
+function Sensor() {
+
+}
+Sensor.protype.getReading = function() {
+
+}
+Sensor.protype.getReading = function() {
+
+}
+Sensor.protype.getReading = function() {
+
+}
+Sensor.protype.getReading = function() {
+
+}
+Sensor.protype.getReading = function() {
+
+}
+Sensor.protype.getReading = function() {
+
+}
+Sensor.protype.getReading = function() {
+
+}
